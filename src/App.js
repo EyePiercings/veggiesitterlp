d74d55ef72307e1460d3bad48a28e7f2e18ce865
EyePiercings <doshawareb@gmail.com>
2018-12-29 15:56:04 -0500

    Redesigned website. Second Iteration.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
 import Main from './components/main';
-import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
-import { Link } from "react-router-dom";
+import Logo from './media/LOGO-VEGGIE-SITTER.png';
+import {Link} from 'react-router-dom';
+import './App.css';
 
-class App extends Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
-    <div className="demo-big-content">
-      <Layout>
-          <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'black'}} to= '/'>Veggie Sitter</Link>} scroll>
-              <Navigation style={{color: 'black'}}>
-                  <Link to="/about">About</Link>
-              </Navigation>
-          </Header>
-          <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to= '/'>Veggie Sitter</Link>}>
-              <Navigation>
-                  <Link to="/about">About</Link>
-              </Navigation>
-          </Drawer>
-          <Content>
-              <div className="page-content" />
-              <Main />
-          </Content>
-          
-      </Layout>
-  </div>
+      <div>
+        <Navbar color="transparent" light expand="md">
+          <NavbarBrand href="/"><img  src={Logo} style={{height: '30%', width: '30%'}} alt="VeggieSitter" /></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink tag={Link} to="/about/">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/contact/">Contact</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/#">Follow</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/#">Subscribe</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <Main />
+        
+      </div>
     );
   }
 }
-
-export default App;
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,46 +1,53 @@
 import React, { Component } from "react";
-import {Grid, Cell, Button} from 'react-mdl';
+//import {Grid, Cell, Button} from 'react-mdl';
+import { Container, Row, Col, Button } from "reactstrap";
 
-class Landing extends Component {
+class LandingPage extends Component {
   render() {
     return (
-    <div style={{width: '100%', margin: 'auto'}}>
-      <Grid className="landing-grid">
+   <div style={{width: '100%', margin: 'auto'}}>
+      <Container className="landing-grid">
       {/* Specifies the width of the page for the grid */}
-        <Cell col={12}>
-          <div className="banner-text">
-          <h4>
-            who are we?
-          </h4>
-          <hr id="title-separator" />
-            <p>
-              Veggie Sitter is an upcoming mobile gardening application which introduces container gardening to users via our purchasable non-gmo seeds, push notifications, gps tracking, weather systems, and A.I. plant recognition. We seek to democratize the agricultural industry and teach the benefits of gardening on social health and the environment.
-              <br />
-              We're changing black thumbs into green ones one seed at a time!
-            </p>
-            <br />
-            <div style={{margin: '25px', marginTop: '50px', color: '#33CC66', fontSize: '25px'}}>
+        <Row>
+          <Col xs='3'>
+            <h1 id="opening-text">
+              turn your black thumb into a <br/>green one
+            </h1>
+            <p id="under-opening"> 
+          Veggie sitter is a upcoming mobile gardening application which introducees container gardening to users via our purchasable non-gmo seeds, push notifications, gps tracking, weather systems, and AI plant recognition. We seek to democratize the agricultural industry and teach the benefits of gardening on social health and environmental health. We’re chainging black thumbs into green ones one seed at a time.
+          </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="banner-text">
+          
+            <div style={{textAlign: 'center', marginTop: '50px', color: '#33CC66', fontSize: '30px'}}>
                 <strong>"nature and nurture"</strong>
             </div>
+            
+          </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
             {/**
               When we've developed a video, I will insert it into this div here.
             */}
-            <div className="video-box"></div>
+            <div className="video-box" />
 
             <hr />
 
             <Button raised ripple>
               Subscribe
             </Button>
-          </div>
-
-        </Cell>
-
-      </Grid>  
+          </Col>
+        </Row>
+      </Container>  
     </div>
     );
 
   }
 }
 
-export default Landing;
+export default LandingPage;
